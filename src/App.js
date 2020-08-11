c569873093322354a94659129795efd90573cad0
Andrés Pérez <11398826+Oxyrus@users.noreply.github.com>
2020-08-10 21:40:52 -0500

    Finish writing app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import Boiler from "./components/Boiler";
 
 const App = () => {
   return (
-    <>
-      <h1>Yogurt</h1>
-      <IngredientContainer ingredientName="Leche" quantity="1L" />
-      <IngredientContainer ingredientName="Fermento" quantity="1Kg" />
-      <Boiler />
-    </>
+    <main class="min-h-screen h-full mx-auto w-3/5 flex items-center text-gray-700">
+      <div class="grid grid-cols-2 gap-4">
+        <div>
+          <div className="py-4">
+            <IngredientContainer ingredientName="Leche" />
+          </div>
+          <div className="py-4">
+            <IngredientContainer ingredientName="Fermento" />
+          </div>
+        </div>
+        <div>
+          <Boiler />
+        </div>
+      </div>
+    </main>
   );
 };
 
